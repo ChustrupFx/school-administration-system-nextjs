0a2667f50c73a879dd14a0a2f08a3fc44f032af6
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-02-03 11:56:44 -0300

    sidebar of home page created

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,3 +1,47 @@
-export default () => {
-    
+import style from './style.module.css';
+import { Apps } from '@material-ui/icons';
+import { useAuth } from '../../context/Auth';
+
+const Home = () => {
+    const currentYear = new Date().getFullYear();
+    const { user } = useAuth();
+
+    return (
+        <>
+            <div className={style.sidebar}>
+                <div>
+                    <h1 className={style.schoolTitle}>Escola</h1>
+                </div>
+                <div className={style.studentInfo}>
+                    <div className={style.studentMeta}>
+                        <img className={style.studentImg} src="/images/default-user-image.png"></img>
+                        <div>
+                            <p>{user.name}</p>
+                            <p>EM 3ºB - Matutino</p>
+                            <p>Ensino Médio</p>
+                        </div>
+                    </div>
+                    <div className={style.studentRegistration}>
+                        <p>Matrícula: 02591</p>
+                        {currentYear}
+                    </div>
+                </div>
+                <hr />
+                <div>
+                    <nav>
+                        <ul className={style.navList}>
+                            <li className={style.navItem}>
+                                <a className={style.itemLink}>
+                                    <Apps />
+                                    <span>INÍCIO</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </>
+    );
 }
+
+export default Home;
