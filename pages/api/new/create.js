07b321393683bb11b5b7a2fb328ee27dfc64ba8e
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-02-09 21:51:25 -0300

    Create news api

---
+++ pages/api/new/create.js
@@ -0,0 +1,16 @@
+const News = require('../../../models/News');
+
+const handler = async (req, res) => {
+    const { body } = req;
+
+    try {
+        const news = await News.create(body);
+
+        return res.json({ ok: true, news });
+    } catch (e) {
+        return res.json({ errorMsg: 'Erro ao criar uma notícia nova.' });
+    }
+
+}
+
+export default handler;
