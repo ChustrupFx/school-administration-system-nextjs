582a92def1eacfa60e7ed5f0b58c21a979c2c6ff
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-02-11 19:43:35 -0300

    Added som allow headers

--- services/api/index.js
+++ services/api/index.js
@@ -7,7 +7,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Cookie, Set-Cookie, Authorization'
     }
 });
 
