a74efa7ba94c4022380b7bcca334e02a57ca1f4d
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-02-11 15:11:23 -0300

    Added pagination to user/[id]/tasks api

--- pages/api/user/[id]/tasks/index.js
+++ pages/api/user/[id]/tasks/index.js
@@ -5,6 +5,9 @@ const User = require('../../../../../models/User');
 const handler = async (req, res) => {
     const { query: { id } } = req;
 
+    const limit = parseInt(req.query.limit) || 0;
+    const page = parseInt((req.query.page - 1) * limit) || 0;
+
     try {
         const user = await User.findById(id)
             .populate('degree');
@@ -12,7 +15,9 @@ const handler = async (req, res) => {
         if (!user)
             return res.json({ errorMsg: 'User not found.' });
 
-        const tasks = await Task.find({ degree: user.degree._id });
+        const tasks = await Task.find({ degree: user.degree._id })
+                                .skip(page)
+                                .limit(limit);
 
         return res.json({ ok: true, tasks });
     } catch (e) {
