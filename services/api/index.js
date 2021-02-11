efb6f1ed6e1a218bfa95080bef2bb042f943babf
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-02-11 19:37:09 -0300

    Added some CORS headers

--- services/api/index.js
+++ services/api/index.js
@@ -5,7 +5,9 @@ const api = axios.create({
     baseURL: 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     }
 });
 
