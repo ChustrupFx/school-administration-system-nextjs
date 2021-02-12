704f88cf4b2467a78237914d09ff809fa64af126
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-02-12 00:40:16 -0300

    Axios base URLchanged

--- services/api/index.js
+++ services/api/index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const cookie = require('cookie');
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
